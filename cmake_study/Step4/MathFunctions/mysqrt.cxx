766acc2c122b39fefd36e8ac0c072ab260280474
helloShangHai <zhang_shuai777@163.com>
2020-09-12 17:45:02 +0800

    Adding System Introspection

--- cmake_study/Step4/MathFunctions/mysqrt.cxx
+++ cmake_study/Step4/MathFunctions/mysqrt.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 // a hack square root calculation using simple operations
 double mysqrt(double x)
@@ -7,7 +8,13 @@ double mysqrt(double x)
     return 0;
   }
 
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+  double result = exp(log(x) * 0.5);
+  std::cout << "Computing sqrt of " << x << " to be " << result
+            << " using log and exp" << std::endl;
+#else
   double result = x;
+#endif
 
   // do ten iterations
   for (int i = 0; i < 10; ++i) {
