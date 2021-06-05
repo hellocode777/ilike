a81e9466c73430637a0fe48d3efde779422ef585
helloShangHai <zhang_shuai777@163.com>
2021-06-05 15:35:35 +0800

    update emacs configuration

---
+++ .emacs.d/lisp/init-better-defaults.el
@@ -0,0 +1,17 @@
+
+(setq ring-bell-function 'ignore)
+
+(global-auto-revert-mode t)
+
+(global-display-line-numbers-mode t)
+
+(setq make-backup-files nil)
+
+(setq auto-save-default nil)
+
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+(provide 'init-better-defaults)
---
+++ .emacs.d/lisp/init-org.el
@@ -0,0 +1,9 @@
+
+(require 'org)
+
+(setq org-src-fontify-natively t)
+
+(setq org-agenda-files '("~/org"))
+
+
+(provide 'init-org)
