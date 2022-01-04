0934c112914f7b437c9cdaeee46396db3be2bdac
hellocode777 <zhang_shuai777@163.com>
2022-01-04 21:30:21 +0800

    data_structure

---
+++ data_structure/src/data_structure/data_structure.h
@@ -0,0 +1,11 @@
+#ifndef DATA_STRUCTURE_H_
+#define DATA_STRUCTURE_H_
+
+#include "../../include/public.h"
+
+class DataStructure
+{
+  
+};    
+
+#endif
---
+++ data_structure/src/data_structure_main.cpp
@@ -0,0 +1,8 @@
+#include "data_structure/data_structure.h"
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
+
+
